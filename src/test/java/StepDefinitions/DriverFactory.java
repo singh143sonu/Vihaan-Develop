be1b625836f3352b51cbf3cb6f6147245520c194
Sandeep Bhadauria <sandeep.bhadouria@gmail.com>
2018-02-09 10:16:44 +1300

    Test Commit
    Testing changes.

--- src/test/java/StepDefinitions/DriverFactory.java
+++ src/test/java/StepDefinitions/DriverFactory.java
@@ -25,6 +25,7 @@ public class DriverFactory {
     @AfterMethod
     public static void quitDriver() throws Exception {
         driverThread.get().quitDriver();
-        
+        driverThread.get().quitDriver();
+        driverThread.get().quitDriver();
     }
 }
